1ac0747e094e9df3177c2c182ee1302f0029db2c
congfeng <2120400017@stu.hrbust.edu.cn>
2021-10-01 06:05:15 -0700

    bugfix; first run.

--- KNN/run-demo.sh
+++ KNN/run-demo.sh
@@ -4,5 +4,6 @@ input_data=/demo/CarOwners.csv
 output_dir=/demo/res
 params_file=/demo/KnnParams.txt
 jar_file=/home/cong/Code/demo/target/demo-1.0.jar
+class_name=com.example.KnnPattern
 
-hadoop jar $jar_file KnnPattern $input_data $output_dir $params_file
+hadoop jar $jar_file $class_name $input_data $output_dir $params_file
