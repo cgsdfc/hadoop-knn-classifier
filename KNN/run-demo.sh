d38f25ce2490dc7a0810b03ccfd36083703ad0b7
cgsdfc <cgsdfc@126.com>
2021-10-04 05:50:24 -0700

    fixup.

--- KNN/run-demo.sh
+++ KNN/run-demo.sh
@@ -3,7 +3,7 @@
 # 运行本程序以预测车辆类型。
 
 input_data=/demo/CarOwners.csv # 训练数据集，hdfs中的路径（请确保已经上传）。
-output_dir=/demo/res1 # 输出目录，hdfs中的路径（请确保不存在）。
+output_dir=/demo/result # 输出目录，hdfs中的路径（请确保不存在）。
 params_file=/demo/KnnParams.txt # 配置文件，hdfs中的路径（请确保已经上传）。
 jar_file=/home/cong/Code/demo/target/demo-1.0.jar # 项目构建产生的Jar包（请修改为实际的路径）。
 class_name=com.example.KnnClassifier # 项目主类名，不需要修改。
