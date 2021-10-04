96b54b7b9cec2ab69e77a2efe79269470e16f30f
cgsdfc <cgsdfc@126.com>
2021-10-04 05:13:10 -0700

    add comments.

--- src/main/java/com/example/KnnPattern.java
+++ src/main/java/com/example/KnnPattern.java
@@ -5,6 +5,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.io.File;
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -82,14 +83,15 @@ public class KnnPattern {
         // 算法参数K。
         int K;
 
+        // 保存从配置文件中解析出来的测试实例的字段。
         double normalisedSAge;
         double normalisedSIncome;
         String sStatus;
         String sGender;
         double normalisedSChildren;
 
-        // The known ranges of the dataset, which can be hardcoded in for the purposes
-        // of this example
+        // 有些字段有最大和最小值，可以利用它们把相应的字段正则化。
+        // 正则化可以使总的距离不受某个字段的大小的影响。
         double minAge = 18;
         double maxAge = 77;
         double minIncome = 5000;
@@ -97,17 +99,14 @@ public class KnnPattern {
         double minChildren = 0;
         double maxChildren = 5;
 
-        // Takes a string and two double values. Converts string to a double and
-        // normalises it to
-        // a value in the range supplied to reurn a double between 0.0 and 1.0
+        // 把输入的字符串转化为浮点数，并以输入的最大最小值对其进行正则化。
+        // 正则化后的取值范围为0-1。
         private double normalisedDouble(String n1, double minValue, double maxValue) {
             return (Double.parseDouble(n1) - minValue) / (maxValue - minValue);
         }
 
-        // Takes two strings and simply compares then to return a double of 0.0
-        // (non-identical) or 1.0 (identical).
-        // This provides a way of evaluating a numerical distance between two nominal
-        // values.
+        // 计算两个离散型变量的距离。这里我们简单把距离定义为两个变量是否相等。
+        // 这个定义所产生的距离与上述的正则化产生的距离在值域上是一致的。
         private double nominalDistance(String t1, String t2) {
             if (t1.equals(t2)) {
                 return 0;
@@ -116,47 +115,37 @@ public class KnnPattern {
             }
         }
 
-        // Takes a double and returns its squared value.
+        // 计算一个距离的平方值。
         private double squaredDistance(double n1) {
             return Math.pow(n1, 2);
         }
 
-        // Takes ten pairs of values (three pairs of doubles and two of strings), finds
-        // the difference between the members
-        // of each pair (using nominalDistance() for strings) and returns the sum of the
-        // squared differences as a double.
-        private double totalSquaredDistance(double R1, double R2, String R3, String R4, double R5, double S1, double S2,
-                String S3, String S4, double S5) {
+        // 输入两个实例的数据，其中第一个实例的数据为 R1-R5，第二个实例的数据为 S1-S5，
+        // 计算两个实例的对应属性的距离的平方和。
+        // 对于离散型变量，计算它们的 nominalDistance，对于连续型变量，计算它们的差值。
+        // 此方法返回的是两个实例的距离的平方（综合考虑了所有的属性）。
+        private double totalSquaredDistance(//
+                double R1, double R2, String R3, String R4, double R5, //
+                double S1, double S2, String S3, String S4, double S5) {
             double ageDifference = S1 - R1;
             double incomeDifference = S2 - R2;
             double statusDifference = nominalDistance(S3, R3);
             double genderDifference = nominalDistance(S4, R4);
             double childrenDifference = S5 - R5;
-
-            // The sum of squared distances is used rather than the euclidean distance
-            // because taking the square root would not change the order.
-            // Status and gender are not squared because they are always 0 or 1.
-            return squaredDistance(ageDifference) + squaredDistance(incomeDifference) + statusDifference
-                    + genderDifference + squaredDistance(childrenDifference);
+            // 不需要开平方根，因为它不会改变值的顺序关系。
+            return squaredDistance(ageDifference) + squaredDistance(incomeDifference) //
+                    + statusDifference + genderDifference + squaredDistance(childrenDifference);
         }
 
-        // The @Override annotation causes the compiler to check if a method is actually
-        // being overridden
-        // (a warning would be produced in case of a typo or incorrectly matched
-        // parameters)
+        // 重写 Mapper 的setup方法，初始化本对象的一些数据。
         @Override
-        // The setup() method is run once at the start of the mapper and is supplied
-        // with MapReduce's
-        // context object
         protected void setup(Context context) throws IOException, InterruptedException {
+            // 获取配置文件。
             if (context.getCacheFiles() != null && context.getCacheFiles().length > 0) {
-                // Read parameter file using alias established in main()
-                String knnParams = FileUtils.readFileToString(new File("./knnParamFile"));
+                String knnParams = FileUtils.readFileToString(new File("./knnParamFile"), Charset.defaultCharset());
                 StringTokenizer st = new StringTokenizer(knnParams, ",");
 
-                // Using the variables declared earlier, values are assigned to K and to the
-                // test dataset, S.
-                // These values will remain unchanged throughout the mapper
+                // 获取参数K和测试实例的字段。
                 K = Integer.parseInt(st.nextToken());
                 normalisedSAge = normalisedDouble(st.nextToken(), minAge, maxAge);
                 normalisedSIncome = normalisedDouble(st.nextToken(), minIncome, maxIncome);
@@ -166,15 +155,14 @@ public class KnnPattern {
             }
         }
 
+        // 重写map方法，实现对训练数据的分布式处理。
         @Override
-        // The map() method is run by MapReduce once for each row supplied as the input
-        // data
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            // Tokenize the input line (presented as 'value' by MapReduce) from the csv file
-            // This is the training dataset, R
+            // 对一行csv数据进行解析。训练数据记为r，测试数据记为t，作为变量的前缀以示区分。
             String rLine = value.toString();
             StringTokenizer st = new StringTokenizer(rLine, ",");
 
+            // 解析各个字段的数据。
             double normalisedRAge = normalisedDouble(st.nextToken(), minAge, maxAge);
             double normalisedRIncome = normalisedDouble(st.nextToken(), minIncome, maxIncome);
             String rStatus = st.nextToken();
@@ -182,34 +170,29 @@ public class KnnPattern {
             double normalisedRChildren = normalisedDouble(st.nextToken(), minChildren, maxChildren);
             String rModel = st.nextToken();
 
-            // Using these row specific values and the unchanging S dataset values,
-            // calculate a total squared
-            // distance between each pair of corresponding values.
-            double tDist = totalSquaredDistance(normalisedRAge, normalisedRIncome, rStatus, rGender,
-                    normalisedRChildren, normalisedSAge, normalisedSIncome, sStatus, sGender, normalisedSChildren);
+            // 计算训练实例和测试实例的距离。
+            double tDist = totalSquaredDistance(//
+                    normalisedRAge, normalisedRIncome, rStatus, rGender, normalisedRChildren, // 训练数据。
+                    normalisedSAge, normalisedSIncome, sStatus, sGender, normalisedSChildren); // 测试数据。
 
-            // Add the total distance and corresponding car model for this row into the
-            // TreeMap with distance
-            // as key and model as value.
+            // 更新距离最小的不超过K个实例的记录。
             KnnMap.put(tDist, rModel);
-            // Only K distances are required, so if the TreeMap contains over K entries,
-            // remove the last one
-            // which will be the highest distance number.
+            // 最多保存K个记录。
             if (KnnMap.size() > K) {
                 KnnMap.remove(KnnMap.lastKey());
             }
         }
 
+        // 在map调用结束后，会调用cleanup方法，我们在这里把保存在knnMap中的数据写入Context中。
         @Override
-        // The cleanup() method is run once after map() has run for every row
         protected void cleanup(Context context) throws IOException, InterruptedException {
-            // Loop through the K key:values in the TreeMap
+            // 遍历knnMap，把数据导出到context。
             for (Map.Entry<Double, String> entry : KnnMap.entrySet()) {
                 Double knnDist = entry.getKey();
                 String knnModel = entry.getValue();
-                // distanceAndModel is the instance of DoubleString declared aerlier
+                // 因为是把数据交给hadoop，必须要能够序列化，所以必须使用DoubleString类型。
                 distanceAndModel.set(knnDist, knnModel);
-                // Write to context a NullWritable as key and distanceAndModel as value
+                // key为空，所以NullWritable是一个占位符。
                 context.write(NullWritable.get(), distanceAndModel);
             }
         }
