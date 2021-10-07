20e864097520d8645668588ff7d53d08267c84b0
cgsdfc <cgsdfc@126.com>
2021-10-07 05:58:30 -0700

    bugfix verified.

--- KNN/run-demo.sh
+++ KNN/run-demo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # 运行本程序以预测车辆类型。
 
@@ -8,4 +8,9 @@ params_file=/demo/KnnParams.txt # 配置文件，hdfs中的路径（请确保已
 jar_file=/home/cong/Code/demo/target/demo-1.0.jar # 项目构建产生的Jar包（请修改为实际的路径）。
 class_name=com.example.KnnClassifier # 项目主类名，不需要修改。
 
+# 先确保jar包是最新的。
+mvn package -f "/home/cong/Code/demo/pom.xml"
+
 hadoop jar $jar_file $class_name $input_data $output_dir $params_file
+
+echo $?
