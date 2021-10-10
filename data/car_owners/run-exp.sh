c1e0f12765bcb7f22c1168cace0d2dbe8557d687
cgsdfc <cgsdfc@126.com>
2021-10-10 02:38:59 -0700

    在carowners上进行cv成功。

--- data/car_owners/run-exp.sh
+++ data/car_owners/run-exp.sh
@@ -2,11 +2,11 @@
 
 # 运行本程序以预测车辆类型。
 
-trainingFile=/demo/data/iris/iris_training.csv # 训练数据集，hdfs中的路径（请确保已经上传）。
-configFile=/home/cong/Code/demo/data/iris/KnnParams.json # 配置文件，hdfs中的路径（请确保已经上传）。
+trainingFile=/demo/data/car_owners/CarOwners_train.csv # 训练数据集，hdfs中的路径（请确保已经上传）。
+configFile=/home/cong/Code/demo/data/car_owners/KnnParams.json # 配置文件，hdfs中的路径（请确保已经上传）。
 jarFile=/home/cong/Code/demo/target/demo-1.0.jar # 项目构建产生的Jar包（请修改为实际的路径）。
 mainClass=com.example.KnnExperiment # 项目主类名，不需要修改。
-numFolds=3
+numFolds=5
 
 # 先确保jar包是最新的。
 mvn package -f "/home/cong/Code/demo/pom.xml"
--- src/main/java/com/example/KnnExperiment.java
+++ src/main/java/com/example/KnnExperiment.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -8,16 +12,26 @@ import org.apache.hadoop.fs.Path;
 // 这个类是KnnEvaluator的封装，提供更加高级的接口。
 public class KnnExperiment {
 
+    private static final int numArgs = 3;
+
     public static void main(String[] args) throws Exception {
-        final String datasetPath = "/demo/data/iris/iris_training.csv";
-        final int numFolds = 3;
+        if (args.length != numArgs) {
+            System.err.println("Usage: KnnExperiment <configFile> <numFolds> <trainingFile>");
+            System.exit(3);
+        }
+
+        final String datasetPath = args[2];
+        final int numFolds = Integer.parseInt(args[1]);
         EvalDatasetsGenerator generator = new CrossValidationDataGenerator(numFolds);
-        KnnConfigData configData = new KnnConfigData();
-        configData.k = 5;
-        configData.ds = "iris";
+
+        Gson gson = new Gson();
+        Reader reader = new FileReader(new File(args[0]));
+        KnnConfigData configData = gson.fromJson(reader, KnnConfigData.class);
+
         KnnEvaluator evaluator = new KnnEvaluator(generator, configData, new Path(datasetPath));
         EvaluationResult result = evaluator.doEvaluation();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        gson = new GsonBuilder().setPrettyPrinting().create();
         String string = gson.toJson(result);
         System.out.println(string);
     }
