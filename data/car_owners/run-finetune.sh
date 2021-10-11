c1ee2a0c99851487a3a8c7e0be3201e1d8a20523
cgsdfc <cgsdfc@126.com>
2021-10-11 19:53:41 +0800

    新建了finetune配置。

--- data/car_owners/run-finetune.sh
+++ data/car_owners/run-finetune.sh
@@ -3,12 +3,11 @@
 configFile=/home/cong/Code/demo/data/car_owners/finetune-config.json # 配置文件，hdfs中的路径（请确保已经上传）。
 jarFile=/home/cong/Code/demo/target/demo-1.0.jar # 项目构建产生的Jar包（请修改为实际的路径）。
 mainClass=com.example.KnnFineTune # 项目主类名，不需要修改。
-Kmax=20
 outputFile=/home/cong/Code/demo/data/car_owners/result/finetune-result.json
 
 # 先确保jar包是最新的。
 mvn package -f "/home/cong/Code/demo/pom.xml"
 
-hadoop jar $jarFile $mainClass $configFile $Kmax $outputFile
+hadoop jar $jarFile $mainClass $configFile $outputFile
 
 echo $?
--- src/main/java/com/example/KnnFineTune.java
+++ src/main/java/com/example/KnnFineTune.java
@@ -7,13 +7,17 @@ import com.example.KnnExperiment.*;
 // 实现一个比较基本的调参过程。
 // 在训练集上，通过交叉检验来获取比较好的K值，然后在测试数据集上看性能。
 public class KnnFineTune {
-    private KnnExpConfigData configData;
-    private int Kmin = 1;
-    private int Kstep = 1;
-    private int Kmax = defaultKmax;
 
-    private static final int numArgs = 3;
-    private static final int defaultKmax = 10;
+    private KnnFineTuneConfigData configData;
+    private static final int numArgs = 2;
+
+    public static class KnnFineTuneConfigData {
+        private int Kmin;
+        private int Kstep;
+        private int Kmax;
+        public DatasetInfo dsInfo;
+        public ResampleInfo rsInfo;
+    }
 
     public static class TuningResult {
         public int K;
@@ -27,29 +31,32 @@ public class KnnFineTune {
     }
 
     public static class FineTuneResult {
-        KnnExpConfigData config;
+        KnnFineTuneConfigData config;
         public ArrayList<TuningResult> tuningResults = new ArrayList<>();
         public ArrayList<TestingResult> testingResults = new ArrayList<>();
         public TuningResult bestTuningResult = new TuningResult();
         public TestingResult bestTestingResult = new TestingResult();
 
-        public FineTuneResult(KnnExpConfigData config) {
+        public FineTuneResult(KnnFineTuneConfigData config) {
             this.config = config;
             bestTuningResult.K = bestTestingResult.K = -1;
             bestTuningResult.mean = bestTestingResult.mean = -1;
         }
     }
 
-    public KnnFineTune(KnnExpConfigData configData, int Kmax) {
+    public KnnFineTune(KnnFineTuneConfigData configData) {
         this.configData = configData;
-        this.Kmax = Kmax;
     }
 
     // 对每个K值，通过交叉检验来找到性能最好的。
     private void findBestParams(FineTuneResult fineTuneResult) throws Exception {
-        for (int K = Kmin; K <= Kmax; K += Kstep) {
-            configData.K = K;
-            KnnExperiment experiment = new KnnExperiment(configData);
+        KnnExpConfigData expConfigData = new KnnExpConfigData();
+        expConfigData.dsInfo = this.configData.dsInfo;
+        expConfigData.rsInfo = this.configData.rsInfo;
+
+        for (int K = configData.Kmin; K <= configData.Kmax; K += configData.Kstep) {
+            expConfigData.K = K;
+            KnnExperiment experiment = new KnnExperiment(expConfigData);
             EvaluationResult result = experiment.run();
             TuningResult tuningResult = new TuningResult();
             tuningResult.K = K;
@@ -57,8 +64,7 @@ public class KnnFineTune {
             tuningResult.std = result.std;
 
             fineTuneResult.tuningResults.add(tuningResult);
-            if (tuningResult.mean - tuningResult.std > fineTuneResult.bestTuningResult.mean
-                    + fineTuneResult.bestTuningResult.std) {
+            if (tuningResult.mean > fineTuneResult.bestTuningResult.mean) {
                 fineTuneResult.bestTuningResult = tuningResult;
             }
         }
@@ -90,14 +96,13 @@ public class KnnFineTune {
 
     public static void main(String[] args) throws Exception {
         if (args.length != numArgs) {
-            System.err.println("Usage: KnnFineTune <configFile> <Kmax> <outputFile>");
+            System.err.println("Usage: KnnFineTune <configFile> <outputFile>");
             System.exit(4);
         }
 
-        KnnExpConfigData configData = FsUtils.readFromJsonFormatLocal(args[0], KnnExpConfigData.class);
-        int Kmax = Integer.parseInt(args[1]);
+        KnnFineTuneConfigData configData = FsUtils.readFromJsonFormatLocal(args[0], KnnFineTuneConfigData.class);
         String outputFile = args[2];
-        KnnFineTune fineTune = new KnnFineTune(configData, Kmax);
+        KnnFineTune fineTune = new KnnFineTune(configData);
         FineTuneResult result = fineTune.run();
         FsUtils.writeInJsonFormatLocal(outputFile, result);
         System.out.println(FsUtils.toJsonString(result));
