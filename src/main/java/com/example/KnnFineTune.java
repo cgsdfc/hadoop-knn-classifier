4f692b060f906d22ec85d936ce27d465a5fa44a9
cgsdfc <cgsdfc@126.com>
2021-10-10 21:30:34 -0700

    rename.

--- src/main/java/com/example/KnnFineTune.java
+++ src/main/java/com/example/KnnFineTune.java
@@ -26,14 +26,14 @@ public class KnnFineTune {
         public double mean;
     }
 
-    public static class FinalExpResult {
+    public static class FineTuneResult {
         KnnExpConfigData config;
         public ArrayList<TuningResult> tuningResults = new ArrayList<>();
         public ArrayList<TestingResult> testingResults = new ArrayList<>();
         public TuningResult bestTuningResult = new TuningResult();
         public TestingResult bestTestingResult = new TestingResult();
 
-        public FinalExpResult(KnnExpConfigData config) {
+        public FineTuneResult(KnnExpConfigData config) {
             this.config = config;
             bestTuningResult.K = bestTestingResult.K = -1;
             bestTuningResult.mean = bestTestingResult.mean = -1;
@@ -46,7 +46,7 @@ public class KnnFineTune {
     }
 
     // 对每个K值，通过交叉检验来找到性能最好的。
-    private void findBestParams(FinalExpResult finalExpResult) throws Exception {
+    private void findBestParams(FineTuneResult fineTuneResult) throws Exception {
         for (int K = Kmin; K <= Kmax; K += Kstep) {
             configData.K = K;
             KnnExperiment experiment = new KnnExperiment(configData);
@@ -56,33 +56,33 @@ public class KnnFineTune {
             tuningResult.mean = result.mean;
             tuningResult.std = result.std;
 
-            finalExpResult.tuningResults.add(tuningResult);
-            if (tuningResult.mean - tuningResult.std > finalExpResult.bestTuningResult.mean
-                    + finalExpResult.bestTuningResult.std) {
-                finalExpResult.bestTuningResult = tuningResult;
+            fineTuneResult.tuningResults.add(tuningResult);
+            if (tuningResult.mean - tuningResult.std > fineTuneResult.bestTuningResult.mean
+                    + fineTuneResult.bestTuningResult.std) {
+                fineTuneResult.bestTuningResult = tuningResult;
             }
         }
     }
 
     // 对于每个K值，都到测试集上去跑一下，看性能是不是最好的。
-    private void runOnTestingData(FinalExpResult finalExpResult) throws Exception {
-        for (TuningResult result : finalExpResult.tuningResults) {
+    private void runOnTestingData(FineTuneResult funeTuneResult) throws Exception {
+        for (TuningResult result : funeTuneResult.tuningResults) {
             KnnPredictor predictor = new KnnPredictor(result.K);
             ResultJsonData jsonData = predictor.predict(configData.dsInfo);
             TestingResult testingResult = new TestingResult();
             testingResult.K = result.K;
             testingResult.mean = jsonData.accuracy;
-            finalExpResult.testingResults.add(testingResult);
+            funeTuneResult.testingResults.add(testingResult);
             // 测试集上没有std。
-            if (testingResult.mean > finalExpResult.bestTestingResult.mean) {
-                finalExpResult.bestTestingResult = testingResult;
+            if (testingResult.mean > funeTuneResult.bestTestingResult.mean) {
+                funeTuneResult.bestTestingResult = testingResult;
             }
 
         }
     }
 
-    public FinalExpResult run() throws Exception {
-        FinalExpResult finalExpResult = new FinalExpResult(this.configData);
+    public FineTuneResult run() throws Exception {
+        FineTuneResult finalExpResult = new FineTuneResult(this.configData);
         findBestParams(finalExpResult);
         runOnTestingData(finalExpResult);
         return finalExpResult;
@@ -98,7 +98,7 @@ public class KnnFineTune {
         int Kmax = Integer.parseInt(args[1]);
         String outputFile = args[2];
         KnnFineTune fineTune = new KnnFineTune(configData, Kmax);
-        FinalExpResult result = fineTune.run();
+        FineTuneResult result = fineTune.run();
         FsUtils.writeInJsonFormatLocal(outputFile, result);
         System.out.println(FsUtils.toJsonString(result));
     }
